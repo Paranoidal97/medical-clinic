079653f5adbf188fbe1c3e67cf58cbb2a47cc416
Paranoidal97 <LukaszRams1997@gmail.com>
2023-10-01 12:14:17 +0200

    first commit

---
+++ src/main/java/com/paranoidal97/demo/Patient.java
@@ -0,0 +1,15 @@
+package com.paranoidal97.demo;
+
+import java.time.LocalDate;
+
+public class Patient {
+    private String email;
+    private String password;
+    private String idCardNo;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private LocalDate birthday;
+
+
+}
---
+++ src/test/java/com/paranoidal97/demo/MedicalClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paranoidal97.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
